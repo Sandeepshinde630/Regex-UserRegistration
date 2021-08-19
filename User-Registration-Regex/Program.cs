50d59118d1a1bd79b72784fdc61bae017ead0eca
unknown <sandeepshinde630@gmail.com>
2021-08-19 16:24:10 +0530

    feat:Validate FisrtName

--- User-Registration-Regex/Program.cs
+++ User-Registration-Regex/Program.cs
@@ -7,6 +7,9 @@ namespace User_Registration_Regex
         static void Main(string[] args)
         {
             Console.WriteLine("User Registration Regex Demo!");
+            UserRegistration userRegistration = new UserRegistration();
+            bool result =userRegistration.validateUserInfo("Sandeep");
+            Console.WriteLine(result);
         }
     }
 }
---
+++ User-Registration-Regex/UserRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Regex
+{
+    class UserRegistration
+    {
+        string FirstName = "^[A-Z][a-z]{2,}$";
+
+        public bool validateUserInfo(string input)
+        {
+            return Regex.IsMatch(input, FirstName);
+        }
+    }
+}
